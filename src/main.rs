49f843e9b9cf2f121f54fccdd1837ffd230b4eaa
Dylan <dylanblokhuis3@hotmail.com>
2022-08-01 23:03:42 +0200

    0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -48,7 +48,7 @@ async fn main() {
         .fallback(get_service(ServeDir::new(".")).handle_error(handle_error))
         .layer(TraceLayer::new_for_http());
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     tracing::debug!("listening on http://{}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
